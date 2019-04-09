e5e343b25a60dd1e68ce6a4cf1078ea92bc91f82
Reg Maltais <reginald.maltais@gmail.com>
2019-04-09 10:23:37 -0400

    Create setup.py to install dependencies.
    Doesn't define console scripts yet.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(name='blocksequence',
+  version='0.1',
+  description='Sequence a collection of blocks in a parent geography',
+  maintainer='Reginald Maltais',
+  maintainer_email='reginald.maltais2@canada.ca',
+  url='https://github.com/statcan/block-sequence',
+  packages=['blocksequence',],
+  license='MIT',
+  long_description=open('README.md').read(),
+  install_requires=[
+    "click >= 6.7",
+    "click-log >= 0.3.2",
+    "click-plugins >= 1.0.3",
+    "Fiona >= 1.7.13",
+    "geopandas >= 0.4.0",
+    "matplotlib >= 2.2.3",
+    "networkx >= 2.2",
+    "numpy >= 1.16.1",
+    "pandas >= 0.23.4",
+    "pyproj >= 1.9.6",
+    "pytest >= 4.0.1",
+    "Shapely >= 1.6.4.post2",
+  ],
+  entry_points={
+    'console_scripts': []
+  }
+  )
