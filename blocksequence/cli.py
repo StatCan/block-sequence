a28a4aeebb57bbc9ed71c709ea044b7605ec7f71
Reginald Maltais <reginald.maltais2@canada.ca>
2019-11-05 13:09:40 -0500

    Remove old commands that aren't used.

--- blocksequence/cli.py
+++ blocksequence/cli.py
@@ -6,10 +6,8 @@ from dotenv import load_dotenv
 import geopandas as gpd
 import numpy as np
 import pandas as pd
-from sqlalchemy import create_engine
+import sqlalchemy as sa
 
-from .utils import commands as utils
-from .sequence import commands as sequence
 from .blocksequence import BlockSequence
 
 logger = logging.getLogger()
@@ -20,15 +18,11 @@ load_dotenv()
 
 @click.group()
 @click_log.simple_verbosity_option(logger)
-@click.option('--source_host', envvar='SEQ_SOURCE_HOST', help="Source host name")
-@click.option('--source_db', envvar='SEQ_SOURCE_DB', help="Source database name")
-@click.option('--source_user', envvar='SEQ_SOURCE_USER', help="Source DB username")
-@click.option('--source_pass', envvar='SEQ_SOURCE_PASS', help="Source DB password")
-@click.option('--outdb', envvar='SEQ_OUTPUT', default='sequencing.sqlite', 
+@click.option('--outdb', envvar='SEQ_OUTPUT', default='sequencing.sqlite',
   type=click.Path(dir_okay=False, resolve_path=True),
   help="Output SQLite database filename")
 @click.pass_context
-def main(ctx, source_host, source_db, source_user, source_pass, outdb):
+def main(ctx, outdb):
   """Sequence a road network through a geography to aid in enumeration."""
 
   logger.debug("cli.main start")
@@ -36,11 +30,9 @@ def main(ctx, source_host, source_db, source_user, source_pass, outdb):
   # ensure that ctx.obj exists and is a dict
   ctx.ensure_object(dict)
 
-  # initiate the source and output database connections and store those in the context
-  logger.debug("Source DB: {} as {} on {}".format(source_db, source_user, source_host))
-  src_conn = create_engine('postgresql://{user}:{password}@{host}/{db}'.format(db=source_db, user=source_user, password=source_pass, host=source_host))
+  # initiate the output database connections and store those in the context
   logger.debug("Output DB: {}".format(outdb))
-  dest_conn = create_engine('sqlite:///{}'.format(outdb), echo=False)
+  dest_conn = sa.create_engine('sqlite:///{}'.format(outdb), echo=False)
 
   # configure logging on the database engine to match our logger
   # sqlalchemy returns full results with debug, so avoid going to that level
@@ -51,18 +43,10 @@ def main(ctx, source_host, source_db, source_user, source_pass, outdb):
 
   # attach DB information to the context
   ctx.obj = {
-    'src_db': src_conn,
     'dest_db': dest_conn
   }
 
   logger.debug("cli.main end")
-
-# add subcommands
-main.add_command(utils.node_weights)
-main.add_command(sequence.sequence)
-main.add_command(utils.order_blocks)
-main.add_command(utils.t_intersections)
-main.add_command(utils.start_points)
 
 
 @click.command()
@@ -118,7 +102,7 @@ def create_edge_list(ctx, gpkg, parent_geography, parent_geography_uid_field, ch
   # convert our GeoDataFrame to a normal dataframe to be written out
   edges = pd.DataFrame(edge_gdf.drop('geometry', axis=1))
   # write the data, overwriting anything that already exists
-  edges.to_sql('edge_list', ctx['dest_conn'], if_exists='replace')
+  edges.to_sql('edge_list', ctx['dest_db'], if_exists='replace')
 
 
 def get_start_node(line):
@@ -146,7 +130,7 @@ def sequence_blocks(ctx, parent_geography, parent_geography_uid_field, child_geo
     for group_id, group in pgeo_group:
         bs = BlockSequence(group, 'start_node_id', 'end_node_id')
         bs_df = bs.eulerian_circuit(child_geography_uid_field)
-        bs_df.to_sql('sequence', ctx['dest_conn'], if_exists='append')
+        bs_df.to_sql('sequence', ctx['dest_db'], if_exists='append')
 
 
 def start():
