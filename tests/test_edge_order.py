934560bc2f2825b19ef6cbe7e815578092a30f66
Reginald Maltais <reginald.maltais2@canada.ca>
2019-11-22 12:49:25 -0500

    Add a test for a cycle with an edge sticking off one node.

--- tests/test_edge_order.py
+++ tests/test_edge_order.py
@@ -38,3 +38,22 @@ def test_basic_path():
     expected = {(0, 1, 0): 1, (1, 2, 0): 2, (2, 3, 0): 3, (3, 4, 0): 4}
 
     assert labels == expected
+
+
+def test_interior_branch():
+    """Test the labelling of edges in a cycle with an edge sticking off it."""
+
+    # create a small graph and label the edges
+    g = nx.cycle_graph(5, create_using=nx.MultiGraph)
+    # edge order is going to look for a sequence field to determine the start node
+    seq = {(0, 1, 0): 0, (1, 2, 0): 1, (2, 5, 0): 2, (2, 5, 1): 3, (2, 3, 0): 4, (3, 4, 0): 5, (0, 4, 0): 6}
+    nx.set_edge_attributes(g, seq, 'sequence')
+
+    # initialize the edge order and get the labels
+    eo = EdgeOrder(g)
+    labels = eo.label_edges()
+
+    # the labels that should have been produced
+    expected = {(0, 1, 0): 1, (1, 2, 0): 2, (2, 5, 0): 3, (2, 5, 1): 4, (2, 3, 0): 5, (3, 4, 0): 6, (0, 4, 0): 7}
+
+    assert labels == expected
